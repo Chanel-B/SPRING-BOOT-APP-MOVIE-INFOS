a01af8d083aad1b67d511cde424afcfba46db52f
Chanel Boris <chanelboris@gmail.com>
2021-08-07 15:25:06 +0200

    Movie category service added
    From spring init we added the web dependency and then generated the project

---
+++ movie-category-service/src/main/java/com/chanel/moviecategoryservice/MovieCategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chanel.moviecategoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCategoryServiceApplication.class, args);
+	}
+
+}
---
+++ movie-category-service/src/test/java/com/chanel/moviecategoryservice/MovieCategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanel.moviecategoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
