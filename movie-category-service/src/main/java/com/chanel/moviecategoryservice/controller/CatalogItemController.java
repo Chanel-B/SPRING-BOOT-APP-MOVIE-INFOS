1425f0b25003bd5aed721a02e993f7590a13116c
Chanel Boris <chanelboris@gmail.com>
2021-08-08 07:05:10 +0200

    Created communication between Movie Catalog and Movie  Info

--- movie-category-service/src/main/java/com/chanel/moviecategoryservice/controller/CatalogItemController.java
+++ movie-category-service/src/main/java/com/chanel/moviecategoryservice/controller/CatalogItemController.java
@@ -1,9 +1,14 @@
 package com.chanel.moviecategoryservice.controller;
 
 import com.chanel.moviecategoryservice.model.CatalogItem;
+import com.chanel.moviecategoryservice.model.Movie;
+import com.chanel.moviecategoryservice.model.Rating;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -11,8 +16,24 @@ public class CatalogItemController {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(String userId){
-        return Collections.singletonList(
-          new CatalogItem("Transform", "Test", 4)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        //let's create a list of
+        //lets assume this is response we got from a user who watches the movies
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("4567", 3)
         );
+
+        //for each rating we replace the rating with a catalogitem
+        //just had coding it by making a list and return it back
+        return ratings.stream().map(rating -> {
+
+            Movie movie = restTemplate.getForObject( "http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+
+            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList()); //then collect to a list and return it back
     }
 }
